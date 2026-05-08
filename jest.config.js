3303e3191a21e13aa72039580b07f25b2f547b64
liujunxiang0076 <liujunxiang0076@users.noreply.github.com>
2026-05-09 03:18:23 +0800

    ci: 移除 jest.setup.js，简化测试配置

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,5 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)',
   ],
-  setupFiles: ['./jest.setup.js'],
   maxWorkers: 1,
 };
